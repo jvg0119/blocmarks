a69f91a53d28dd7199c380c7cb8b92fb4ea01cb1
Joseph <jvg_0119@yahoo.com>
2017-02-18 22:29:36 -0800

    modified incomingController 3rd time

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -8,35 +8,26 @@ class IncomingController < ApplicationController
 		#body_plain = params["stripped-text"]
 
 	 # Find the user by using params[:sender]
-	 #@user = User.find_by(email: params[:sender])
-	 @user = User.find_or_create_by(email: params[:sender])
-	 if @user.nil? 
-	 		@user.password = "password"
-	 		@user.skip_confirmation!
-	 		@user.save!
-
+	 # Check if user is nil, if so, create and save a new user
 	 @user = User.find_by(email: params[:sender])
-	 if @user.nil?
-	 		@user = User.new(email: params[:sender], password: "password")
-	 		@user.skip_confirmation!
-	 		@user.save!
+		 if @user.nil?
+		 		@user = User.new(email: params[:sender], password: "password")
+		 		@user.skip_confirmation!
+		 		@user.save!
+		 	end
 
    # Find the topic by using params[:subject]
-   # @topic = Topic.find_by(title: params[:subject])
+   # Check if the topic is nil, if so, create and save a new topic
    @topic = Topic.find_by(title: params[:subject])
      if @topic.nil?
      	 @topic.user = @user
      	 @topic = Topic.new(title: params[:subject], user: @user) 
      	 @topic.save!
+     end
 
    # Assign the url to a variable after retreiving it from params["body-plain"]
-   @url = params["body-plain"]
-
-   # Check if user is nil, if so, create and save a new user
-
-   # Check if the topic is nil, if so, create and save a new topic
-
    # Now that you're sure you have a valid user and topic, build and save a new bookmark
+   @url = params["body-plain"]
 
    @bookmark = @topic.bookmarks.new(url: @url)
    @bookmark.save!
