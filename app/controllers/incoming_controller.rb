007ca88f0589ca9591ff46c56dc398690e839735
Joseph <jvg_0119@yahoo.com>
2017-02-18 16:39:26 -0800

    6 rec inc emails add incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -7,6 +7,25 @@ class IncomingController < ApplicationController
 		#sender = params["sender"]
 		#body_plain = params["stripped-text"]
 
+
+	 # Find the user by using params[:sender]
+	 @user = User.find_by(email: params[:sender])
+
+   # Find the topic by using params[:subject]
+   @topic = Topic.find_by(title: params[:subject])
+   
+   # Assign the url to a variable after retreiving it from params["body-plain"]
+   @url = params["body-plain"]
+
+   # Check if user is nil, if so, create and save a new user
+
+   # Check if the topic is nil, if so, create and save a new topic
+
+   # Now that you're sure you have a valid user and topic, build and save a new bookmark
+
+   @bookmark = @topic.bookmarks.new(url: @url, user: @user)
+   @bookmark.save!
+
 		head 200
 	end
 
